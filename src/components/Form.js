303e28962081135b4f56273b00b12170657eafca
juanperezu <juanperezu@gmail.com>
2020-09-23 21:48:36 -0500

    Modificado react

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,11 +12,11 @@ return(
 </div>
 <div className="form-group">
 <label>Usuario</label>
-<input type="text"  className="form-control" placeholder="Su usuario"/>
+<input type="text"  className="form-control" placeholder="Su usuario.."/>
 </div>
 <div className="form-group">
 <label>Clave</label>
-<input type="password"  className="form-control" placeholder="Su clave"/>
+<input type="password"  className="form-control" placeholder="Su clave.."/>
 </div>
 <div className="form-group">
 <button type="submit" className="btn btn-primary espacio"> Ingresar</button>
